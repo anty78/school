bc3134a363ad7df6b2e6527dcd9bca149e7aeb8a
anty78 <38722657+anty78@users.noreply.github.com>
2020-04-13 14:13:47 +0300

    Upload file of array

---
+++ name_1.pas
@@ -0,0 +1,17 @@
+﻿Program name_1;
+Var a:array[1..10]of integer;
+i:integer;
+s:real;
+Begin
+WriteLn('Сейчас массив заполниться случайными числами от 1 до 100');
+for i:=1 to 10 do
+begin
+a[i]:=Random(100);
+WriteLn('a[',i,']=',a[i]);
+end;
+s:=0;
+for i:=1 to 10 do
+s:=s+a[i];
+s:=s/10;
+WriteLn('Среднее арефметическое массива=',s);
+END.
